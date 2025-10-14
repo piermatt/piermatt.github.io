7a202a110edf68bd2eb7285716d7e39bff3c3c12
piermatt <79876575+piermatt@users.noreply.github.com>
2025-10-14 16:49:27 +0000

    Deploying to gh-pages from @ piermatt/piermatt.github.io@a33bc13c5749675157594b9e6cf1c8d677455ae9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-projects",
           title: "projects",
-          description: "My cool projects.",
+          description: "My current and realized projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
