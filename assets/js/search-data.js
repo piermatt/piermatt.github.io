8e068f525f3d7a7870d9b01debb08985c9b093ac
piermatt <79876575+piermatt@users.noreply.github.com>
2025-10-14 16:01:19 +0000

    Deploying to gh-pages from @ piermatt/piermatt.github.io@d1d714fae0d2361a462373a3a4ee15962841c9a8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Materials for courses.",
+          description: "Materials for courses. Under development",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
